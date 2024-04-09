92ba5f36ab4c3bfc148361fd2179dd8688694283
Mohamed Elrfaay <56503517+mohamedeld@users.noreply.github.com>
2024-04-10 00:32:24 +0200

    add event details:

--- helpers/utils-events.js
+++ helpers/utils-events.js
@@ -10,7 +10,7 @@ export async function getAllEvents(){
       ...data[key]
     })
   }
-  console.log(allEvents);
+  
   return allEvents;
 }
 
--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,16 +1,16 @@
 import EventContent from '@/components/event-detail/event-content';
 import EventLogistics from '@/components/event-detail/event-logistics';
 import EventSummary from '@/components/event-detail/event-summary';
-import { getEventById } from '@/dummy-data';
-import { useRouter } from 'next/router'
+import { getAllEvents, getEventById } from '@/helpers/utils-events';
+
 import React from 'react'
 
-export default function EventDetailPage() {
-  const router = useRouter();
-  const {id} = router.query;
- const event = getEventById(id);
- if(!event || !id){
-  return <p>Not Found event with this id {id}</p>
+export default function EventDetailPage(props) {
+  
+  
+  const event = props.event;
+ if(!event){
+  return <p>Not Found event with this id {event.id}</p>
  }
   return (
     <>
@@ -21,4 +21,22 @@ export default function EventDetailPage() {
       </EventContent>
     </>
   )
+}
+export async function getStaticProps(context){
+  const eventId = context.params.id;
+  const event = await getEventById(eventId);
+  return {
+    props:{
+      event:event
+    },
+    revalidate:30
+  }
+}
+export async function getStaticPaths(){
+  const allEvents = await getAllEvents();
+  const paths = allEvents.map(event=> ({params:{id:event.id}}));
+  return {
+    paths:paths,
+    fallback:false
+  }
 }
